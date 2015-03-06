d7c422938ecf3eee25753b65770dd52bff2c2f45
Stefan Walther <swr@qlik.com>
2015-03-06 22:17:54 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+> Nothing to read, yet
+
+## Author
+
+** Stefan Walther**
+
+* [qliksite.io](http://qliksite.io)
+* [twitter/waltherstefan](http://twitter.com/waltherstefan)
+* [github.com/stefanwalther](http://github.com/stefanwalther)
+
+## License & Copyright
+The software is made available "AS IS" without any warranty of any kind under the MIT License (MIT).
+
+See [Additional license information for this solution.](LICENSE.md)
---
+++ tests/index.js
@@ -0,0 +1,49 @@
+'use strict';
+var chai = require( 'chai' );
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect();
+var extInfo = require( './../lib/extinfo' );
+var path = require( 'path' );
+
+describe( 'extinfo', function () {
+
+	it( 'should return basic information for a single file', function ( done ) {
+
+		extInfo.fromFile( path.join( __dirname, './fixtures/basic/sample1.qext' ) )
+			.then( function ( data ) {
+
+				data.should.not.be.empty;
+				assert( typeof(data) === 'object' );
+				data.should.have.deep.property( 'version', '0.0.1' );
+				data.should.have.deep.property( 'name', 'Sample 1' );
+				done();
+
+			} )
+			.catch( function ( data ) {
+				console.error( 'Error:', data );
+				assert( false );
+				done();
+			} );
+
+	} );
+
+	it( 'should return exinfo properly (by dir)', function ( done ) {
+
+		extInfo.fromFolder( path.join( __dirname, './fixtures/basic/' ) )
+			.then( function ( data ) {
+				data.should.not.be.empty;
+				assert( typeof(data) === 'object' );
+				data.should.have.deep.property( 'version', '0.0.1' );
+				data.should.have.deep.property( 'name', 'Sample 1' );
+				done();
+
+			} )
+			.catch( function ( data ) {
+				console.error( 'Error: ', data );
+				assert( false );
+				done();
+			} );
+	} );
+
+} );

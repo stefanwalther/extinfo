21f2cc358bdc241c20b89d3cf64914f34c170587
Stefan Walther <swr@qlik.com>
2015-05-14 00:34:47 +0200

    ...

--- test/index.js
+++ test/index.js
@@ -8,42 +8,60 @@ var path = require( 'path' );
 
 describe( 'extinfo', function () {
 
-	it( 'should return basic information for a single file', function ( done ) {
+    it( 'should return basic information for a single file', function ( done ) {
 
-		extInfo.fromFile( path.join( __dirname, './fixtures/basic/sample1.qext' ) )
-			.then( function ( data ) {
+        extInfo.fromFile( path.join( __dirname, './fixtures/basic/sample1.qext' ) )
+            .then( function ( data ) {
 
-				data.should.not.be.empty;
-				assert( typeof(data) === 'object' );
-				data.should.have.deep.property( 'version', '0.0.1' );
-				data.should.have.deep.property( 'name', 'Sample 1' );
-				done();
+                data.should.not.be.empty;
+                assert( typeof(data) === 'object' );
+                data.should.have.deep.property( 'version', '0.0.1' );
+                data.should.have.deep.property( 'name', 'Sample 1' );
+                done();
 
-			} )
-			.catch( function ( data ) {
-				console.error( 'Error:', data );
-				assert( false );
-				done();
-			} );
+            } )
+            .catch( function ( data ) {
+                console.error( 'Error:', data );
+                assert( false );
+                done();
+            } );
 
-	} );
+    } );
 
-	it( 'should return exinfo properly (by dir)', function ( done ) {
+    it( 'should return exinfo properly (by dir without /)', function ( done ) {
 
-		extInfo.fromDir( path.join( __dirname, './fixtures/basic/' ) )
-			.then( function ( data ) {
-				data.should.not.be.empty;
-				assert( typeof(data) === 'object' );
-				data.should.have.deep.property( 'version', '0.0.1' );
-				data.should.have.deep.property( 'name', 'Sample 1' );
-				done();
+        extInfo.fromDir( path.join( __dirname, './fixtures/basic' ) )
+            .then( function ( data ) {
+                data.should.not.be.empty;
+                assert( typeof(data) === 'object' );
+                data.should.have.deep.property( 'version', '0.0.1' );
+                data.should.have.deep.property( 'name', 'Sample 1' );
+                done();
 
-			} )
-			.catch( function ( data ) {
-				console.error( 'Error: ', data );
-				assert( false );
-				done();
-			} );
-	} );
+            } )
+            .catch( function ( data ) {
+                console.error( 'Error: ', data );
+                assert( false );
+                done();
+            } );
+    } );
+
+    it( 'should return exinfo properly (by dir with /)', function ( done ) {
+
+        extInfo.fromDir( path.join( __dirname, './fixtures/basic/' ) )
+            .then( function ( data ) {
+                data.should.not.be.empty;
+                assert( typeof(data) === 'object' );
+                data.should.have.deep.property( 'version', '0.0.1' );
+                data.should.have.deep.property( 'name', 'Sample 1' );
+                done();
+
+            } )
+            .catch( function ( data ) {
+                console.error( 'Error: ', data );
+                assert( false );
+                done();
+            } );
+    } );
 
 } );
